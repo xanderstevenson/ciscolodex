544d28f4ab83073610e4c13e1ac9b26c36f0c3e7
Alexander Stevenson -T (alexstev - COLLABERA INC at Cisco) <alexstev@cisco.com>
2020-04-24 14:27:26 -0500

    On page one, clicking button of question will reveal answer

--- Ciscolodex.py
+++ Ciscolodex.py
@@ -39,18 +39,24 @@ class StartPage(tk.Frame):
 
 class PageOne(tk.Frame):
     def __init__(self, master):
+
+        def show_text(self, item):
+            item.pack(side="top", pady=10)
+        # def hide_text(self, item):
+        #     item.pack_forget()       
+
         tk.Frame.__init__(self, master)
         tk.Frame.configure(self, bg='blue')
         tk.Label(self, width=50, text="Access", fg="blue", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
-        
-        var1 = StringVar()
-        var1.set("I have a Cisco account with my old company that has all my Cisco certifications and \n I no longer have access to that email. How can I get the DevAsc course and exam connected to my new email?")
-        tk.Label(self, width=95, textvariable=var1, fg="black", font=('Helvetica', 12)).pack(side="top", pady=10)
-        
-        var1b = tk.Text(self, height=10, width=100, fg="black", font=('Helvetica', 10))
-        var1b.pack()
 
-        quote="""Great question. We'll just need to merge your accounts. We'll need three things from you: \
+
+        var1 = StringVar()
+        var1_text="I have a Cisco account with my old company that has all my Cisco certifications and \n I no longer have access to that email. How can I get the DevAsc course and exam connected to my new email?"
+        var1.set(var1_text)
+        
+        var1b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var1b.pack()
+        var1b_text="""Great question. We'll just need to merge your accounts. We'll need three things from you: \
         \n 1. The old email associated with your account \n 2. The new email your using for Cisco now \n 3. The  Profile ID \
         \n Here's how you can get your Profile ID: \
         \n Open an Incognito window in Chrome or new Private window in Firefox or Safari. Go enter your sign-on information. \
@@ -58,16 +64,19 @@ class PageOne(tk.Frame):
         \n Click Login andile icon in the upper right-hand corner and choose Profile & Achievements. \
         \n Copy the profile ID, such as d47d4b96-6812-11e7-a515-02420ae9710c, which should be in the URL and on the page. \
         \n Paste your profile ID into the ticket or other communication to DevNet."""
-        var1b.insert(tk.END, quote) 
+        var1b.insert(tk.END, var1b_text) 
         # fg="black", font=('Helvetica', 12)).pack(side="top", pady=10, fill="both")
-                
-        var2 = StringVar()
-        var2.set("How does a Cisco employee get access?")
-        tk.Label(self, width=55, textvariable=var2, fg="black", font=('Helvetica', 12)).pack(side="top", pady=10)        
+        label = Button(self, width=95, textvariable=var1, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var1b)).pack(side="top", pady=10)        
 
-        var2b = tk.Text(self, height=10, width=100, fg="black", font=('Helvetica', 10))
-        var2b.pack()
-        quote2 = """The DevNet Associate Fundamentals Course is not available for Cisco Employees to purchase at this time. \
+
+        var2 = StringVar()
+        var2_text="How does a Cisco employee get access?"
+        var2.set(var2_text)
+         
+
+        var2b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var2b.pack()
+        var2b_text = """The DevNet Associate Fundamentals Course is not available for Cisco Employees to purchase at this time. \
         \n If you'd like to express interest in this course, you may fill out this form: \
         \n (https://app.smartsheet.com/b/form/e7fdfeb3f6074674a577ae52fffe0a34A) \
         \n If you would like to start your learning journey for the DevNet Associate Certification immediately, \
@@ -75,10 +84,35 @@ class PageOne(tk.Frame):
         \n The title of the course is 'Developing Applications and Automating Workflows using Cisco Core Platforms (DEVASC) v1.0' \
         \n and it is available today at no cost on the Cisco Digital Learning Library (CDLL). Please visit https://cxtraining.cisco.com/c/cdll_infbundle/ .\
         \n Please stay tuned for more information on the DevNet Associate Fundamentals training course employee program."""
-        var2b.insert(tk.END, quote2)
+        var2b.insert(tk.END, var2b_text)
+        label = Button(self, width=95, textvariable=var2, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var2b)).pack(side="top", pady=10) 
+
+
+
+        var3 = StringVar()
+        var3_text="How does a Cisco employee get access?"
+        var3.set(var3_text)
+        label = Button(self, width=95, textvariable=var2, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var3b)).pack(side="top", pady=10)        
+
+        var3b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var3b.pack()
+        var3b_text = """The DevNet Associate Fundamentals Course is not available for Cisco Employees to purchase at this time. \
+        \n If you'd like to express interest in this course, you may fill out this form: \
+        \n (https://app.smartsheet.com/b/form/e7fdfeb3f6074674a577ae52fffe0a34A) \
+        \n If you would like to start your learning journey for the DevNet Associate Certification immediately, \
+        \n we’d like to direct you to another DevNet Associate course that has been developed in partnership with Learning@Cisco. \
+        \n The title of the course is 'Developing Applications and Automating Workflows using Cisco Core Platforms (DEVASC) v1.0' \
+        \n and it is available today at no cost on the Cisco Digital Learning Library (CDLL). Please visit https://cxtraining.cisco.com/c/cdll_infbundle/ .\
+        \n Please stay tuned for more information on the DevNet Associate Fundamentals training course employee program."""
+        var3b.insert(tk.END, var3b_text)
+
+
 
         tk.Button(self, text="Go back to start page",
                   command=lambda: master.switch_frame(StartPage)).pack(pady=10)
+
+
+
 
 class PageTwo(tk.Frame):
     def __init__(self, master):
