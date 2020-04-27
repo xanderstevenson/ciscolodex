f7e717483b380c99543962f9ff5efd986001ab3f
Alexander Stevenson -T (alexstev - COLLABERA INC at Cisco) <alexstev@cisco.com>
2020-04-27 10:56:21 -0500

    Basic functionality for first two pages. Saving before I try to place gradient bgs on each page"

--- Ciscolodex.py
+++ Ciscolodex.py
@@ -5,12 +5,13 @@ try:
 except:
     import tkinter as tk
     from tkinter import *
-
+from PIL import ImageTk,Image
 
 class SampleApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self._frame = None
+
         self.switch_frame(StartPage)
 
 
@@ -42,14 +43,20 @@ class PageOne(tk.Frame):
 
         def show_text(self, item):
             item.pack(side="top", pady=10)
+            self.Text.set("")
+            self.Text.pack_forget(width=0)
         # def hide_text(self, item):
         #     item.pack_forget()       
+
+
+        background_image=tk.PhotoImage('\img\blue-tile.png')
+        background_label = tk.Label( image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        background_label.image = background_image
 
         tk.Frame.__init__(self, master)
         tk.Frame.configure(self, bg='blue')
         tk.Label(self, width=50, text="Access", fg="blue", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
-
-
         var1 = StringVar()
         var1_text="I have a Cisco account with my old company that has all my Cisco certifications and \n I no longer have access to that email. How can I get the DevAsc course and exam connected to my new email?"
         var1.set(var1_text)
@@ -70,20 +77,13 @@ class PageOne(tk.Frame):
 
 
         var2 = StringVar()
-        var2_text="How does a Cisco employee get access?"
+        var2_text="I can't take my DevAsc exam because of COVID-19 and my course is about to expire. Can I have the course extended?"
         var2.set(var2_text)
          
 
         var2b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
         # var2b.pack()
-        var2b_text = """The DevNet Associate Fundamentals Course is not available for Cisco Employees to purchase at this time. \
-        \n If you'd like to express interest in this course, you may fill out this form: \
-        \n (https://app.smartsheet.com/b/form/e7fdfeb3f6074674a577ae52fffe0a34A) \
-        \n If you would like to start your learning journey for the DevNet Associate Certification immediately, \
-        \n we’d like to direct you to another DevNet Associate course that has been developed in partnership with Learning@Cisco. \
-        \n The title of the course is 'Developing Applications and Automating Workflows using Cisco Core Platforms (DEVASC) v1.0' \
-        \n and it is available today at no cost on the Cisco Digital Learning Library (CDLL). Please visit https://cxtraining.cisco.com/c/cdll_infbundle/ .\
-        \n Please stay tuned for more information on the DevNet Associate Fundamentals training course employee program."""
+        var2b_text = """We are able to extend the course for those effected by COVID-19 but for an indeterminate \n amount of time AND once we do that, you'll lose all progress in the course."""
         var2b.insert(tk.END, var2b_text)
         label = Button(self, width=95, textvariable=var2, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var2b)).pack(side="top", pady=10) 
 
@@ -92,7 +92,7 @@ class PageOne(tk.Frame):
         var3 = StringVar()
         var3_text="How does a Cisco employee get access?"
         var3.set(var3_text)
-        label = Button(self, width=95, textvariable=var2, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var3b)).pack(side="top", pady=10)        
+        label = Button(self, width=45, textvariable=var3, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var3b)).pack(side="top", pady=10)        
 
         var3b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
         # var3b.pack()
@@ -108,19 +108,82 @@ class PageOne(tk.Frame):
 
 
 
+        tk.Button(self, text="Refresh this page",
+                  command=lambda: master.switch_frame(PageOne)).pack(pady=5, side = BOTTOM)
         tk.Button(self, text="Go back to start page",
-                  command=lambda: master.switch_frame(StartPage)).pack(pady=10)
-
+                  command=lambda: master.switch_frame(StartPage)).pack(pady=5,side = BOTTOM)
 
 
 
 class PageTwo(tk.Frame):
     def __init__(self, master):
+
+        def show_text(self, item):
+            item.pack(side="top", pady=10)
+            self.Text.set("")
+            self.Text.pack_forget(width=0)
+        # def hide_text(self, item):
+        #     item.pack_forget()       
+
         tk.Frame.__init__(self, master)
-        tk.Frame.configure(self,bg='red')
-        tk.Label(self, text="Content", fg="red", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        tk.Frame.configure(self, bg='red')
+        tk.Label(self, width=50, text="Content", fg="red", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+      
+      
+        var1 = StringVar()
+        var1_text="I found an error in the course. How can I report it?"
+        var1.set(var1_text)
+        
+        var1b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var1b.pack()
+        var1b_text="""Please open a ticket here --> https://devnetsupport.cisco.com/hc/en-us/requests/new?ticket_form_id=360002862214"""
+        var1b.insert(tk.END, var1b_text) 
+        # fg="black", font=('Helvetica', 12)).pack(side="top", pady=10, fill="both")
+        label = Button(self, width=95, textvariable=var1, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var1b)).pack(side="top", pady=10)        
+
+
+        var2 = StringVar()
+        var2_text="I'm having trouble with a particular lab in the DevNet Associate Fundamentals course. Can someone help me?"
+        var2.set(var2_text)
+         
+
+        var2b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var2b.pack()
+        var2b_text = """For any questions on content, please go to the DevNet Associate community forum here ----> https://learningnetwork.cisco.com/s/topic/0TO3i0000008jY5GAI/devnet-certifications-community"""
+        var2b.insert(tk.END, var2b_text)
+        label = Button(self, width=95, textvariable=var2, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var2b)).pack(side="top", pady=10) 
+
+
+
+        var3 = StringVar()
+        var3_text="How long can you complete the course, without any programming experience?"
+        var3.set(var3_text)
+        label = Button(self, width=85, textvariable=var3, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var3b)).pack(side="top", pady=10)        
+
+        var3b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var3b.pack()
+        var3b_text = """It varies from person to person. If you’d like to improve your coding skills before taking the course, /n we offer these free modules, plus many more: https://developer.cisco.com/startnow/#coding-apis-v0"""
+        var3b.insert(tk.END, var3b_text)
+
+
+        var4 = StringVar()
+        var4_text="Unable to access repo-https://github.com/CiscoDevNet/devasc-code-examples.git -1"
+        var4.set(var4_text)
+        label = Button(self, width=85, textvariable=var4, fg="black", font=('Helvetica', 12), command=lambda: show_text(self, var4b)).pack(side="top", pady=10)        
+
+        var4b = tk.Text(self, height=10, width=120, fg="black", font=('Helvetica', 10))
+        # var3b.pack()
+        var4b_text = """???"""
+        var4b.insert(tk.END, var4b_text)
+
+
+
+        tk.Button(self, text="Refresh this page",
+                  command=lambda: master.switch_frame(PageTwo)).pack(pady=5, side = BOTTOM)
         tk.Button(self, text="Go back to start page",
-                  command=lambda: master.switch_frame(StartPage)).pack()
+                  command=lambda: master.switch_frame(StartPage)).pack(pady=5,side = BOTTOM)
+
+
 
 class PageThree(tk.Frame):
     def __init__(self, master):
